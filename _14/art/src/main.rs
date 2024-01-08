b4590309352e7dfdd341e4b5ccff8ea73e458fab
NOHI <thisisnohi@163.com>
2024-01-08 13:00:25 +0800

    ch14 cargo

---
+++ _14/art/src/main.rs
@@ -0,0 +1,13 @@
+use art::{mix, PrimaryColor};
+
+//! # Art
+//!
+//! A library for modeling artistic concepts.
+//! ch14 car使用测试
+
+fn main() {
+    println!("Hello, world!");
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red, yellow);
+}
